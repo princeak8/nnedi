e8cef066f8efee6cff2cdb87fb1a723f8eb59abc
Michaels042 <mnwachukwu042@gmail.com>
2022-08-22 12:10:13 +0100

    home page

--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useRef, useState, useEffect } from "react";
+
 import RelatedPost from "../components/RelatedPost";
 import styled from "styled-components";
 import photo from "../assets/photo.jpg";
@@ -35,6 +37,10 @@ const ReadMore = styled.div`
 
 function BlogPost({ postItem }) {
   const navigate = useNavigate();
+//   const [state, setState] = useState({
+//     id: '', title: '', content:'', tags:{}, comments:[], comments_count:0, reader:{}, 
+//     coverImage:{}, created_at:'', views_count:0, loading: false, errorMsg: '', loaded: false, loadedSuccessfully: false
+// });
   const commentsCount = () => {
       let commentCount = postItem.comments.length;
       if(commentCount > 0) {
@@ -44,7 +50,23 @@ function BlogPost({ postItem }) {
       return 0
   }
   //console.log("postItem", commentsCount());
-  return (
+
+//   const getPost = async () => {
+//     setState({...state, loading: true});
+//     let post = await request('GET', '/post/show/'+post_id);
+//     setState({...state, loading: false, loaded: true});
+//     if(post.status == 200) {
+//         const {
+//             comments, comments_count, reader, content, coverImage, created_at, tags, id, title, views_count,
+//         } = post.data;
+//         setState({...state, id, title, content, tags, comments, comments_count, reader, coverImage, created_at, views_count, loadedSuccessfully: true});
+//     }else{
+//         console.log('error: ', post.message);
+//         setState({...state, errorMsg: post.message});
+//     }
+// } 
+
+    return (
       <div>
         {/* <Link to="/RelatedPost"><h3>{postItem.title}</h3></Link> */}
           <div className="some-title" onClick={() => navigate(`posts/${postItem.id}`, { state: postItem.id })} style={{cursor:"pointer"}}>
@@ -61,6 +83,8 @@ function BlogPost({ postItem }) {
               <div className="tilte-grid">
                   <div onClick={() => navigate(`posts/${postItem.id}`, { state: postItem.id })} style={{cursor:"pointer"}}>
                     <img src="images/1.jpg" alt=" " />
+                    {/* <Image src={state.coverImage.url} alt="photo of a post" /> */}
+
                   </div>
                   <p className="vel"><Link to="single.html">Phasellus vel arcu vitae neque sagittis aliquet ac at purus.
                   Vestibulum varius eros in dui sagittis non ultrices orci hendrerit.
